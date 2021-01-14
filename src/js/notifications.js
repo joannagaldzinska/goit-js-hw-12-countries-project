4e8bacb681987a622ee91013a92d29dff07f4d51
Joanna Galdzinska <joanna.galdzinska@gmail.com>
2021-01-14 12:45:51 +0200

    hw ready 1st trial

---
+++ src/js/notifications.js
@@ -0,0 +1,16 @@
+
+import  { notice, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+// function showNotice(message) {
+//   text: message,
+//   delay: 5000,
+// };
+
+// const showError = error({
+//     text: 'Oups! It seems like the country you are looking for does not exist. Make sure you typed the name correctly and try again.',
+//     delay: 5000,
+//   });
+
+  export {notice, error};
